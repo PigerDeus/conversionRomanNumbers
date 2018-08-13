5570a3ce90e2641ae8b47e912c8501213a18da6a
PigerDeus <PigerDeus@gmail.com>
2018-08-14 01:28:25 +0300

    complete conversion algorithm

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from app import app
 from views import home
-# @app.route('/')
-# def index():
-#     return '1234567890'
+
     
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port = 5000)
+    
--- views/home.py
+++ views/home.py
@@ -3,11 +3,17 @@ from flask import render_template, request, redirect
 
 alphabet = {
 	'I':1,
+    'IV':4,
 	'V':5,
+    'IX':9,
 	'X':10,
+    'XL':40,
 	'L':50,
+    'XC':90,
 	'C':100,
+    'CD':400,
 	'D':500,
+    'CM':900,
 	'M':1000
 }
 def isRomanNumber(s):
@@ -19,35 +25,58 @@ def isRomanNumber(s):
 			return False
 	return True
 
-def arabic_to_roman(s):
-	pass
+def get_key(d, value):
+    for k, v in d.items():
+        if v == value:
+            return k
+
+# Sort dictyonary 'alphabet' by values
+# Create string from remainder of division
+# and create string roman number by module
+def arabic_to_roman(input_number):
+    num = int(input_number)
+    alf_r = sorted(alphabet.values(), reverse = True)
+    res = ''
+    for k in alf_r:
+        if num //k !=0:
+            res += get_key(alphabet, k)* (num // k)
+        num %= k
+            
+    return res
 
 
-def roman_to_arabic(input_text, ):
+def roman_to_arabic(input_text):
     input_text = input_text.upper()
     res = 0
-    for d in range(len(input_text)):
-        try:
-            if alphabet[input_text[d]] >= alphabet[input_text[d+1]] :
-                res += alphabet[input_text[d]]
+    i = 0
+    while i < len(input_text):
+        s1 = alphabet[input_text[i]]
+        if (i+1 < len(input_text)):
+            s2 = alphabet[input_text[i+1]]
+            if (s1 >= s2):
+                res = res + s1
+                i = i + 1
             else:
-                res -= alphabet[input_text[d]]
-        except IndexError:
-            res += alphabet[input_text[d]]
-    return res 
+                res = res + s2 - s1
+                i = i + 2
+        else:
+            res = res + s1
+            i = i + 1
+    return res
+
+
 
 
 @app.route('/', methods= ['POST', 'GET'])
 def index():
     if request.method == 'POST' and len(request.form['inText'])>0:
-        try:
+        if request.form['inText'].isdigit():
             inStr = int(request.form['inText'])
-            return render_template('index.html', result = 123456789 )
-        except:
-            if isRomanNumber(request.form['inText'].upper()):
-                return render_template('index.html', result = roman_to_arabic(request.form['inText']) )
-            else:
-                return render_template('index.html', result = 'NaN' )
-        return render_template('index.html', result = request.form['inText'])
+            return render_template('index.html', result = arabic_to_roman(request.form['inText']) )
+    
+        elif isRomanNumber(request.form['inText'].upper()):
+            return render_template('index.html', result = roman_to_arabic(request.form['inText']) )
+        else:
+            return render_template('index.html', result = 'NaN' )
     else:
         return render_template('index.html')
